fcbae494a03cde50ae31e68c78daa56ab342c175
tfrank <tfrank@vmware.com>
2023-06-08 08:54:21 -0400

    Initial project.

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using BlazorOnTap.Server.Models;
+
+namespace BlazorOnTap.Server.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(
+        DbContextOptions options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    {
+    }
+}
