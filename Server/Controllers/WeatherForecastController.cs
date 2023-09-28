c778312334803743158e5bc0a851c3fcf137e83b
tfrank <tfrank@vmware.com>
2023-09-28 09:33:23 -0400

    Secure the api using Keycloak.

--- Server/Controllers/WeatherForecastController.cs
+++ Server/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WasmHosted.Shared;
 
@@ -5,6 +6,7 @@ namespace WasmHosted.Server.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[Authorize]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,45 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Keycloak.AuthServices.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+var host = builder.Host;
+host.ConfigureKeycloakConfigurationSource("keycloak.json");
+
 // Add services to the container.
+var services = builder.Services;
+services.AddKeycloakAuthentication(configuration);
+services.AddAuthorization();
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+services.AddControllersWithViews();
+services.AddRazorPages();
 
-builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+
+services.AddSwaggerGen(c =>
+{
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Auth",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.OpenIdConnect,
+        OpenIdConnectUrl = new Uri("http://localhost:8080/realms/test/.well-known/openid-configuration"),
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = "Bearer",
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {securityScheme, Array.Empty<string>()}
+    });
+});
 
 var app = builder.Build();
 
@@ -36,5 +68,8 @@ app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.UseSwagger().UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
