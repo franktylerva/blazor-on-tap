546b3a66105452053f654e85e55093e7f97c5b16
tfrank <tfrank@vmware.com>
2023-09-28 10:36:28 -0400

    Secure the client using Keycloak.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WasmHosted.Client;
 
@@ -6,6 +7,34 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+RegisterHttpClient(builder);
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.MetadataUrl = "http://localhost:8080/realms/test/.well-known/openid-configuration";
+    options.ProviderOptions.Authority = "http://localhost:8080/realms/test";
+    options.ProviderOptions.ClientId = "test-client";
+    options.ProviderOptions.ResponseType = "id_token token";
+
+    options.UserOptions.NameClaim = "preferred_username";
+    options.UserOptions.RoleClaim = "roles";
+    options.UserOptions.ScopeClaim = "scope";
+});
+
+builder.Services.AddApiAuthorization();
 
 await builder.Build().RunAsync();
+
+static void RegisterHttpClient(
+    WebAssemblyHostBuilder builder)
+{
+    var httpClientName = "Default";
+
+    builder.Services.AddHttpClient(httpClientName,
+            client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+        .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+    builder.Services.AddScoped(
+        sp => sp.GetRequiredService<IHttpClientFactory>()
+            .CreateClient(httpClientName));
+}
--- Server/Controllers/WeatherForecastController.cs
+++ Server/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ public class WeatherForecastController : ControllerBase
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+    
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,6 +24,8 @@ public class WeatherForecastController : ControllerBase
     [HttpGet]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.LogInformation("User requesting weather is: " + User.Identity?.Name);
+        
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
